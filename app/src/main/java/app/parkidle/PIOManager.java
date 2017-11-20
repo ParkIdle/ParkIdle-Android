07f81df14e22b64a13b02ad7738a3943158508d0
lim996 <simonestaffa96@gmail.com>
2017-11-20 10:16:48 +0100

    AMQP Libraries linked

--- app/src/main/java/app/parkidle/PIOManager.java
+++ app/src/main/java/app/parkidle/PIOManager.java
@@ -1,18 +1,9 @@
 package app.parkidle;
 
-import android.Manifest;
 import android.app.Application;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.LocalBroadcastManager;
 import android.widget.Toast;
-
 import io.predict.PIOTripSegment;
 import io.predict.PIOZone;
 import io.predict.PredictIO;
