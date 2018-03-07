f75a7571cbb69f55d583a0864909230ba2809af8
Andrea Misuraca <33042194+misu666@users.noreply.github.com>
2018-03-07 20:18:13 +0100

    noConnection button added

--- app/src/main/java/app/parkidle/NoConnectionActivity.java
+++ app/src/main/java/app/parkidle/NoConnectionActivity.java
@@ -1,8 +1,11 @@
 package app.parkidle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by misui on 06/03/2018.
@@ -15,5 +18,13 @@ public class NoConnectionActivity extends AppCompatActivity {
         actionBar.hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_connection);
+        final Button button = findViewById(R.id.buttonNoConn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent tutorial = new Intent(NoConnectionActivity.this,LoginActivity.class);
+                tutorial.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(tutorial);
+            }
+        });
     }
 }
