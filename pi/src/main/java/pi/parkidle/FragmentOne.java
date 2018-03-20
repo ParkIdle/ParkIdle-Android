68525ecd2dcf9a2c582b37a6ed3f9a286fd5962d
mrPsycox <mrPsycox@users.noreply.github.com>
2018-03-20 21:05:03 +0100

    Added some functions for compress the image

--- pi/src/main/java/pi/parkidle/FragmentOne.java
+++ pi/src/main/java/pi/parkidle/FragmentOne.java
@@ -1,8 +1,13 @@
 package pi.parkidle;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,17 +20,93 @@ import android.widget.ImageView;
 public class FragmentOne extends android.support.v4.app.Fragment{
     public ViewPager viewPager;
 
+
+
     public FragmentOne(){
 
     }
-    @Nullable
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        /*String info_funzione_parcheggio = "IMMAGINE FUNZIONE PARCHEGGIO: 886x940: Ora il tipo dell'immagine: .image/png";
+        String immagine_codificata = "FRAGMENT ONE: BYTE USATI CON DECODIFICA: 22962272 , BYTE ALLOCATI: 22962272";
+        //183.437 byte*/
+
 
         View v = inflater.inflate(R.layout.fragment_one_layout,container,false);
-
         super.onViewCreated(v, savedInstanceState);
+
+        /*ImageView funzione_parcheggio = v.findViewById(R.id.imageView4);
+
+
+        //Bitmap funzione_park_decoded = decodeSampledBitmapFromResource(getResources(),R.drawable.funzione_parcheggio,302,451);
+        Bitmap funzione_park_decoded = decodeSampledBitmapFromResource(getResources(),R.drawable.funzione_parcheggio,302,451);
+        if(funzione_park_decoded!=null) {
+            funzione_parcheggio.setImageBitmap(funzione_park_decoded);
+            int byte_spesi = funzione_park_decoded.getByteCount();
+            int byte_allocati = funzione_park_decoded.getAllocationByteCount();
+
+            Log.w("FRAGMENT ONE","BYTE USATI CON DECODIFICA: "+byte_spesi+" , BYTE ALLOCATI: "+byte_allocati);
+
+        }*/
+        /*BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(),R.drawable.tasto_avanti,options);
+        int imageHeight = options.outHeight;
+        int imageWidth = options.outWidth;
+        String imageType = options.outMimeType;
+        Log.w("FRAGMENT ONE","IMMAGINE FUNZIONE PARCHEGGIO: " + imageHeight+ "x" +imageWidth +": Ora il tipo dell'immagine: ."+imageType);
+        Log.w("FRAGMENT ONE","IMMAGINE FUNZIONE PARCHEGGIO: " + imageHeight +"x" +imageWidth +": Ora il tipo dell'immagine: ."+imageType);
+        Log.w("FRAGMENT ONE","IMMAGINE FUNZIONE PARCHEGGIO: " + imageHeight+"x" +imageWidth +": Ora il tipo dell'immagine: ."+imageType);
+        Bitmap funzione_parcheggio = decodeSampledBitmapFromResource(getResources(),R.drawable.tasto_avanti,302,451);
+        if(funzione_parcheggio != null){
+            int byte_spesi = funzione_parcheggio.getByteCount();
+            int byte_allocati = funzione_parcheggio.getAllocationByteCount();
+
+            Log.w("FRAGMENT ONE","BYTE USATI CON DECODIFICA: "+byte_spesi+" , BYTE ALLOCATI: "+byte_allocati);
+        }*/
+
 
 
         ImageView tasto = v.findViewById(R.id.tasto_avanti_1);
--- pi/src/main/java/pi/parkidle/FragmentTwo.java
+++ pi/src/main/java/pi/parkidle/FragmentTwo.java
@@ -1,7 +1,11 @@
 package pi.parkidle;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +21,66 @@ public class FragmentTwo extends android.support.v4.app.Fragment {
     }
     @Nullable
 
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View v2 = inflater.inflate(R.layout.fragment_two_layout,container,false);
-
         super.onViewCreated(v2, savedInstanceState);
+
+        /*BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(),R.drawable.timing_parcheggi , options);
+        int imageHeight = options.outHeight;
+        int imageWidth = options.outWidth;
+        String imageType = options.outMimeType;
+        Log.w("FRAGMENT TWO","IMMAGINE FUNZIONE PARCHEGGIO: " + imageHeight+ "x" +imageWidth +": Ora il tipo dell'immagine: ."+imageType);
+        Log.w("FRAGMENT TWO","IMMAGINE FUNZIONE PARCHEGGIO: " + imageHeight +"x" +imageWidth +": Ora il tipo dell'immagine: ."+imageType);
+        Log.w("FRAGMENT TWO" ,"IMMAGINE FUNZIONE PARCHEGGIO: " + imageHeight+"x" +imageWidth +": Ora il tipo dell'immagine: ."+imageType);
+        Bitmap timing_parcheggi_decoded = decodeSampledBitmapFromResource(getResources(),R.drawable.timing_parcheggi,262,341);
+        if(timing_parcheggi_decoded!= null){
+            int byte_spesi = timing_parcheggi_decoded.getByteCount();
+            int byte_allocati = timing_parcheggi_decoded.getAllocationByteCount();
+            Log.w("FRAGMENT TWO","Immagine decodificata: Byte spesi: " + byte_spesi + " , Byte allocati : "+ byte_allocati);
+        }*/
 
 
         ImageView tasto2 = v2.findViewById(R.id.tasto_avanti_2);
